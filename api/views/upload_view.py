c69607efaea63a77690566b6a43c4d044f374149
henriquechang <henrique.queiroz@stone.com.br>
2022-01-29 17:13:04 -0300

    Integrate controller on view to upload file

--- api/views/upload_view.py
+++ api/views/upload_view.py
@@ -1,14 +1,26 @@
 from rest_framework import status
-
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_422_UNPROCESSABLE_ENTITY
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from api.controllers.upload_controller import UploadController
+from api.handlers.upload_error_handler import UploadErrorHandler
 from api.serializers.upload_serializer import UploadSerializer
+from api.services.s3_service import S3Service
 
 
 class UploadView(APIView):
 
+    s3_service = S3Service()
+    upload_error_handler = UploadErrorHandler()
+    upload_controller = UploadController(
+        s3_service=s3_service,
+        error_handler=upload_error_handler
+    )
+
     def post(self, request, format=None):
+
+        self.upload_error_handler.errors.clear()
 
         serializer = UploadSerializer(data=request.data)
 
@@ -19,4 +31,8 @@ class UploadView(APIView):
             )
 
         file = request.FILES['file']
-        return Response()
+        uploaded = self.upload_controller.send_to_buckets(file)
+        if uploaded:
+            return Response(status=HTTP_204_NO_CONTENT)
+        errors = self.upload_error_handler.errors
+        return Response(data={"errors": errors}, status=HTTP_422_UNPROCESSABLE_ENTITY)
